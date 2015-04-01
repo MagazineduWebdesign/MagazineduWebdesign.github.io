e5c0a123366ba0a24f5924aab3121982ca9f38af
magazineduwebdesign <hey@magazineduwebdesign.com>
2015-04-01 21:29:38 +0200

    remove gif support for grunt responsive_images
    CF. Release history 0.1.4
    https://github.com/andismith/grunt-responsive-images#release-history

--- Gruntfile.js
+++ Gruntfile.js
@@ -284,7 +284,7 @@ module.exports = function (grunt) {
       server: {
         files: [{
           expand: true,
-          src: ['img/**/*.{gif,jpg,png}'],
+          src: ['img/**/*.{jpg,png}'],
           cwd: '<%= yeoman.app %>',
           custom_dest: '<%= yeoman.app %>/img/{%= name %}'
         }]
