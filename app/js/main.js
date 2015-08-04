e6f7ebc9d74934f84b9b416ce6c11df359324d45
MagazineduWebdesign <hey@magazineduwebdesign.com>
2015-08-04 14:15:18 +0200

    fix issue with retina img

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,14 @@ $(document).foundation({
 	  is_hover: false,
 	  mobile_show_parent_link: false, // will copy parent links into dropdowns for mobile navigation
 	  scrolltop : false // jump to top when sticky nav menu toggle is clicked
-	}
+	},
+	interchange: {
+    named_queries : {
+      smallretina : 'only screen and (min-width: 1px) and (-webkit-min-device-pixel-ratio: 2), only screen and (min-width: 1px) and (min--moz-device-pixel-ratio: 2), only screen and (min-width: 1px) and (-o-min-device-pixel-ratio: 2/1), only screen and (min-width: 1px) and (min-device-pixel-ratio: 2), only screen and (min-width: 1px) and (min-resolution: 192dpi), only screen and (min-width: 1px) and (min-resolution: 2dppx)',
+      mediumretina : 'only screen and (min-width: 641px) and (-webkit-min-device-pixel-ratio: 2), only screen and (min-width: 641px) and (min--moz-device-pixel-ratio: 2), only screen and (min-width: 641px) and (-o-min-device-pixel-ratio: 2/1), only screen and (min-width: 641px) and (min-device-pixel-ratio: 2), only screen and (min-width: 641px) and (min-resolution: 192dpi), only screen and (min-width: 641px) and (min-resolution: 2dppx)',
+      largeretina : 'only screen and (min-width: 1025px) and (-webkit-min-device-pixel-ratio: 2), only screen and (min-width: 1025px) and (min--moz-device-pixel-ratio: 2), only screen and (min-width: 1025px) and (-o-min-device-pixel-ratio: 2/1), only screen and (min-width: 1025px) and (min-device-pixel-ratio: 2), only screen and (min-width: 1025px) and (min-resolution: 192dpi), only screen and (min-width: 1025px) and (min-resolution: 2dppx)'
+    }
+  }
 });
 $('#header').headroom();
 $('img').VimeoThumb();
